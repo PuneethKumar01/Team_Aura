8f982497f6610e7408c7bce00e0db0e148a5feac
SEVANTH A G <sevanthrao1406@gmail.com>
2026-03-13 02:08:23 +0530

    SOS Feature implemented

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Components Index
- * 
- * Barrel export for all components
- */
-
-export { SOSButton } from './SOSButton';
-export type { default as SOSButtonProps } from './SOSButton';
--- src/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Hooks Index
- * 
- * Barrel export for all custom hooks
- */
-
-export { useSOSButton } from './useSOSButton';
-export type { default as UseSOSButtonReturn } from './useSOSButton';
